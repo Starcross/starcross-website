1af8e334f3f17f5207ecdbe20642c4219d18ff39
Alex Luton <aluton@2db.co.uk>
2018-02-09 14:38:20 +0000

    Upgrade to Django 2.0.2

--- starcross/urls.py
+++ starcross/urls.py
@@ -1,30 +1,45 @@
-from django.conf.urls import patterns, include, url, static
+"""The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path, re_path, include
 from django.contrib import admin
-from django.views.debug import default_urlconf
 from django.conf import settings
+from django.views.static import serve
 from starcross.views import StaticView, IndexView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'starcross.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    path('', IndexView.as_view()),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('gallery/', include('gallery.urls', namespace='gallery')),
+    path('goingout/', include('goingout.urls', namespace='goingout')),
+    path('tinymce/', include('tinymce.urls')),
+    path('captcha/', include('captcha.urls')),
+    path('<page_name>.html', StaticView.as_view())
+]
 
-    url(r'^$', IndexView.as_view()),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'^gallery/', include('gallery.urls', namespace='gallery')),
-    url(r'^goingout/', include('goingout.urls', namespace='goingout')),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^captcha/', include('captcha.urls')),
-    url(r'^(?P<page_name>.+\.html)$',StaticView.as_view()),
-
-)
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
-    urlpatterns += patterns('',
-        (r'media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT}))
-    urlpatterns += patterns('',
-        (r'static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT}))
+    urlpatterns += [re_path(
+        r'media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT})]
+    urlpatterns += [re_path(
+        r'static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT})]
+    # Debug toolbar
+    import debug_toolbar
+    urlpatterns += [
+        re_path('^__debug__/', include(debug_toolbar.urls))
+    ]
 
--- starcross/views.py
+++ starcross/views.py
@@ -3,12 +3,14 @@ from django.template import TemplateDoesNotExist
 from django.http import Http404
 from blog.views import BlogEntryList
 
+
 class IndexView(BlogEntryList):
     template_name = 'index.html'
 
+
 class StaticView(TemplateView):
     def get(self, request, page_name, *args, **kwargs):
-        self.template_name = page_name
+        self.template_name = "{0}.html".format(page_name)
         response = super(StaticView, self).get(request, *args, **kwargs)
         try:
             return response.render()
