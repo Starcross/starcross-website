8cb7eb9ea39b1e516919c613141aedc9117f64bb
starcross <aluton@gmail.com>
2020-08-30 13:42:01 +0100

    Add rotating backup script for both databases using docker containers and env vars

---
+++ db_backup/db_backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Perform db backups with rotation
+
+TIMESTAMP="$(date +%Y-%m-%d)"
+
+cd "$(dirname "$0")";          
+
+docker exec starcross_mysql_1 sh -c 'exec mysqldump --all-databases -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD"' > mysql-$TIMESTAMP.sql 
+docker exec starcross_postgres_1 sh -c 'exec pg_dump -U"$POSTGRES_USER"' > postgres-$TIMESTAMP.sql
+
+
+TO_DELETE="$(date -d "$TIMESTAMP - 100 days" +%Y-%m-%d)"  
+
+
+if [ -f $DESTINATION/$TO_DELETE ]
+
+then
+   rm -rf $DESTINATION/$TO_DELETE.sql
+
+fi
+
